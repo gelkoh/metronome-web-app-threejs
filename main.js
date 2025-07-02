66b4e8c20c573eff05e88394ba77e4309e869d68
Gelkoh <ole_kirchner@gmx.de>
2025-07-02 22:13:59 +0200

    Add zoom functionality for the 3d object

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import * as THREE from "three";
 
+const ROTATE_SENSITIVITY = 0.01;
+const ZOOM_SENSITIVITY = 0.2;
+const DEFAULT_ZOOM = 6;
+const MAX_ZOOM = 2;
+const MIN_ZOOM = 10;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -17,13 +23,17 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = DEFAULT_ZOOM;
 
 const animate = () => {
     renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
+
+const roundTo = (value, decimals) => {
+    return Math.round(value * 10 ** decimals) / 10 ** decimals;
+}
 
 let lastMouseX, lastMouseY;
 
@@ -34,14 +44,34 @@ const rotateMetronome = (e) => {
         return;
     }
 
-    const diffX = (e.x - lastMouseX) * 0.01;
+    const diffX = (e.x - lastMouseX) * ROTATE_SENSITIVITY;
     cube.rotation.y += diffX;
 
-    const diffY = (e.y - lastMouseY) * 0.01;
+    const diffY = (e.y - lastMouseY) * ROTATE_SENSITIVITY;
     cube.rotation.x += diffY;
 
     lastMouseX = e.x;
     lastMouseY = e.y;
+}
+
+const handleZoom = (e) => {
+    if (e.deltaY < 0) {
+        if (camera.position.z >= MAX_ZOOM && 
+            camera.position.z - ZOOM_SENSITIVITY >= MAX_ZOOM
+        ) {
+            camera.position.z = roundTo(camera.position.z - ZOOM_SENSITIVITY, 2);
+            return;
+        }
+    }
+
+    if (e.deltaY > 0) {
+        if (
+            camera.position.z <= MIN_ZOOM &&
+            camera.position.z + ZOOM_SENSITIVITY <= MIN_ZOOM
+        ) {
+            camera.position.z = roundTo(camera.position.z + ZOOM_SENSITIVITY, 2);
+        }
+    }
 }
 
 renderer.domElement.addEventListener("mousedown", () => {
@@ -49,6 +79,8 @@ renderer.domElement.addEventListener("mousedown", () => {
 });
 
 renderer.domElement.addEventListener("mouseup", () => {
-    renderer.domElement.removeEventListener("mousemove", rotateMetronome)
+    renderer.domElement.removeEventListener("mousemove", rotateMetronome);
     lastMouseX = null, lastMouseY = null;
 });
+
+renderer.domElement.addEventListener("wheel", handleZoom);
